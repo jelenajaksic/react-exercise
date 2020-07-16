8637b88a89e9c4fd7938086b18bf231c9c78bc03
biznisKezual <jelenajaksic@hotmail.co.uk>
2020-07-16 16:20:59 +0200

    Functional Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,19 +1,7 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
-import "bootstrap/dist/css/bootstrap.min.css"
 
-class Dishdetail extends Component {
-
-    formatDate(date) {
-        // const option = { year: 'numeric', month: 'short', day: 'numeric' };
-        // const date1 = new Date(date)
-        // const newdate = date1.toLocaleDateString("en-US", option)
-        // return newdate;
-
-        return new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(date)))
-    }
-
-    renderDish = (dish) => {
+    function RenderDish({dish}) {
         if (dish != null) {
             return (
                 <React.Fragment>
@@ -29,7 +17,7 @@ class Dishdetail extends Component {
                     </div>
                     <div className="col-12 col-md-5 m-1" >
                         <h4 className="mt-3 mb-3">Comments</h4>
-                        {this.renderComments(dish.comments)}
+                        <RenderComments comments={dish.comments}/>
                     </div>
                     </div>
                 </React.Fragment>
@@ -40,13 +28,13 @@ class Dishdetail extends Component {
         }
     }
 
-    renderComments = (comments) => {
+    function RenderComments({comments}) {
         if (comments != null) {
             const com = comments.map(co => {
                 return (
                     <React.Fragment>
                         <li>{co.comment}</li><br />
-                        <li>-- {co.author}, {this.formatDate(co.date)}</li><br />
+                        <li>-- {co.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(co.date)))}</li><br />
                     </React.Fragment>
                 )
             }
@@ -62,14 +50,14 @@ class Dishdetail extends Component {
         }
     }
 
-    render() {
-        const { dish } = this.props;
+
+    const Dishdetail = (props) => {
+        const { dish } = props;
         return (
             <div className="container">
-                {this.renderDish(dish)}
+                <RenderDish dish={props.dish}/>
             </div>
         );
     }
-}
 
 export default Dishdetail;
