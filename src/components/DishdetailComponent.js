435c2f038175a8dfced633248197377205efee35
biznisKezual <jelenajaksic@hotmail.co.uk>
2020-07-24 19:08:05 +0200

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,6 +2,10 @@ import React,{Component} from "react";
 import { Card, CardBody, CardImg, CardText, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Row, Col, Label } from "reactstrap";
 import {Link} from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
+
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
 
     function RenderComments({comments,addComment, dishId})
     {
@@ -30,7 +34,7 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
         }
     }
 
-    function RenderDish({dish}){
+    function RenderDish({dish}) {
         if(dish!=null){
             return(
                 <div className="col-12 col-md-5 m-1">
@@ -52,40 +56,48 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
     }
 
    
-    const Dishdetail = (props)=>{
-          
-        const dish = props.dish;
-        if(dish==null){
+    const Dishdetail = (props) => {
+        if (props.isLoading) {
             return(
-                <div></div>
-            )
-        }
-        return(
-           <div className="container">
-                <Breadcrumb>
-                  <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                  <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                </Breadcrumb>
-                <div className="col-12">
-                    <h3>{props.dish.name}</h3>
-                    <hr/>
+                <div className="container">
+                    <div className="row">
+                        <Loading/>
+                    </div>
                 </div>
-            <div className="row">
-               <RenderDish dish={props.dish} />
-               <RenderComments comments={props.comments} addComment={props.addComment}
-                    dishId={props.dish.id} />
+            );
+        }
+        else if (props.errMess) {
+            return(
+                <div className="container">
+                    <div className="row">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }
+        else if (props.dish != null) {
+            return(
+            <div className="container">
+                    <Breadcrumb>
+                    <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr/>
+                    </div>
+                <div className="row">
+                <RenderDish dish={props.dish} />
+                <RenderComments comments={props.comments} addComment={props.addComment}
+                        dishId={props.dish.id} />
+                </div>
             </div>
-           </div>
-        );
+            );
+        }
     }
 
 export default Dishdetail;
 
-
-
-// CommentForm component
-const maxLength = (len) => (val) => !(val) || (val.length <= len);
-const minLength = (len) => (val) => val && (val.length >= len);
 
 export class CommentForm extends Component {
     constructor(props) {
